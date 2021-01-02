353d152a37cbb0f5e1c8a6b10d1f10afaa5d8bd8
Daniel Larsen <daniel@daniellarsen.com>
2021-01-02 10:47:33 -0600

    Initial Commit

---
+++ CodeLarseny/CodeLarsenyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeLarsenyApp.swift
+//  CodeLarseny
+//
+//  Created by Daniel Larsen on 1/2/21.
+//
+
+import SwiftUI
+
+@main
+struct CodeLarsenyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
